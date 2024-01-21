6ba47fa233b8ccd5be1aa8916a229c62f84805ff
LorenzoDv <lorenzo.devichi@hotmail.fr>
2024-01-21 14:12:28 +0100

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$('#sous-menu').hover(function () {
+    $('.sous-menu').addClass('active-menu');
+});
+// $('#sous-menu').mouseleave(function () {
+//     $('.sous-menu').removeClass('active-menu');
+// });
+$('.sous-menu').mouseleave(function () {
+    $('.sous-menu').removeClass('active-menu');
+});
