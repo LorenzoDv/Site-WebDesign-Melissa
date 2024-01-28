409543dc76b512fbb354bcc8514264b35b816719
LorenzoDv <lorenzo.devichi@hotmail.fr>
2024-01-28 22:19:39 +0100

    Ajout page indentité visuelle

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,31 @@ $('#sous-menu').hover(function () {
 $('.sous-menu').mouseleave(function () {
     $('.sous-menu').removeClass('active-menu');
 });
+
+$('.slidePortefolio').each(function () {
+    $(this).find('.mandala').hover(function () {
+        $(this).addClass('active-slide');
+    });
+    $(this).find('.mandala').mouseleave(function () {
+        $(this).removeClass('active-slide');
+    });
+
+});
+
+$('.tel').mouseenter(function () {
+    $('.tel img').addClass('active-img');
+    $('.tel-number').addClass('active-tel');
+})
+$('.tel').mouseleave(function () {
+    $('.tel img').removeClass('active-img');
+    $('.tel-number').removeClass('active-tel');
+})
+
+$('.mail').mouseenter(function () {
+    $('.mail img').addClass('active-img');
+    $('.mail-adress').addClass('active-mail');
+})
+$('.mail').mouseleave(function () {
+    $('.mail img').removeClass('active-img');
+    $('.mail-adress').removeClass('active-mail');
+})
