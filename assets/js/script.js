1a4eb870f4f7a25d1b80fb4772074f437d0d0a5b
LorenzoDv <lorenzo.devichi@hotmail.fr>
2024-02-13 22:42:08 +0100

    Mise en place du slider plus cdn swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,3 +144,17 @@ function parallax(e) {
 
 
 document.addEventListener('mousemove', parallax);
+
+
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
